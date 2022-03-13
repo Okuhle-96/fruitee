5d59e36517211cb28544d2f025860423545488ef
Okuhle-96 <54100625+Okuhle-96@users.noreply.github.com>
2022-03-13 21:36:32 +0200

    Update fruit-factory.test.js

--- test/fruit-factory.test.js
+++ test/fruit-factory.test.js
@@ -4,28 +4,32 @@ describe('FRUIT LIST FACTORY FUNCTION', function(){
    
         it('should be able to add a fruit to an already existing l8ist', function(){
             let fruitTest = fruitListFactory();
-            fruitTest.addNewFruit('Grapes');
+
+            assert.equal(fruitTest.addNewFruit('Grapes'), 'Grapes');
         });
     });
     
     describe('addNewEmoji function', function(){
         it('should be able to add a fruit emoji', function(){
             let fruitTest = fruitListFactory();
-            fruitTest.addNewFruit('🍇');
+        
+            assert.equal(fruitTest.addNewFruit('🍇'), '🍇');
         });
     });
     
     describe('setFruitsLocalStorage function', function(){
         it('should be able to store a new fruit on the local storage', function(){
             let fruitTest = fruitListFactory();
-            fruitTest.setFruitsLocalStorage('Grapes🍇');
+          
+            assert.equal(fruitTest.setFruitsLocalStorage('Grapes🍇'), 'Grapes🍇')
         });
     });
     
     describe('getFruitsLocalStorage function', function(){
         it('should be able to display a fruit and its emoji from the local storage', function(){
             let fruitTest = fruitListFactory();
-            fruitTest.getFruitsLocalStorage('Grapes🍇');
+            assert.deepEqual(fruitTest.displayFruitFromLocal(["Banana 🍌", "Grapes 🍇", "Melon 🍈", "Watermelon 🍉", "Tangerine 🍊", "Lemon 🍋", "Pineapple 🍍", "Mango 🥭", "Red Apple 🍎"]),
+            ["Banana 🍌", "Grapes 🍇", "Melon 🍈", "Watermelon 🍉", "Tangerine 🍊", "Lemon 🍋", "Pineapple 🍍", "Mango 🥭", "Red Apple 🍎"]);
         });
     });
       
